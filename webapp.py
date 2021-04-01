2882f33b98e2738be1b0d88c18ccdbbc44d3aff5
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-04-01 09:57:13 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('founders.html', options=get_age_options())
 
 def get_age_options():
+    listOfStates = ["1996", "2001", "2014"]
+    options = ListOfStates
+    return options
     
 
 
